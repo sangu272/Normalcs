60ce7179b953e4bed674e1be19ceb89cd6ce2f57
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-10-18 21:42:12 +0530

    Update assistantdatabase.py

--- AnonXMusic/utils/database/assistantdatabase.py
+++ AnonXMusic/utils/database/assistantdatabase.py
@@ -10,8 +10,8 @@
 
 import random
 
-from VIPMUSIC import userbot
-from VIPMUSIC.core.mongo import mongodb
+from AnonXMusic import userbot
+from AnonXMusic.core.mongo import mongodb
 
 db = mongodb.assistants
 
@@ -41,7 +41,7 @@ async def save_assistant(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from VIPMUSIC.core.userbot import assistants
+    from AnonXMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -55,7 +55,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from VIPMUSIC.core.userbot import assistants
+    from AnonXMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -82,7 +82,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from VIPMUSIC.core.userbot import assistants
+    from AnonXMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -95,7 +95,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from VIPMUSIC.core.userbot import assistants
+    from AnonXMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
