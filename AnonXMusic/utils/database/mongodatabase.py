370c978c104a2807df89cd3556180a3f8507ac50
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-10-18 21:43:45 +0530

    Update mongodatabase.py

--- AnonXMusic/utils/database/mongodatabase.py
+++ AnonXMusic/utils/database/mongodatabase.py
@@ -13,7 +13,7 @@ import os
 from typing import Dict, List, Union
 
 import config
-from VIPMUSIC.core.mongo import mongodb
+from AnonXMusic.core.mongo import mongodb
 
 channeldb = mongodb.cplaymode
 commanddb = mongodb.commands
